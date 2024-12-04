390bb9c02c749ca72303aaa5ee4f99d0a9db1c6a
Hazim466 <hazimch466@gmail.com>
2024-12-04 10:18:11 -0800

    Commit Change

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
-app.use(express.json()); // Middleware to parse JSON data
+app.use(express.json()); 
 app.use(bodyParser.json());
-// Connect to MongoDB (replace with your MongoDB URI)
+
 mongoose.connect('mongodb://localhost:27017/BlogApp', {
 }).then(() => {
   console.log('Connected to MongoDB');
@@ -19,12 +19,12 @@ mongoose.connect('mongodb://localhost:27017/BlogApp', {
   console.error('Failed to connect to MongoDB', err);
 });
 
-// Use the user routes for handling user registration
+
 app.use('/user', userRoutes);
 app.use('/blog',blogRoutes);
 app.use('/admin',adminRoutes);
 
-// Starting the server
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
